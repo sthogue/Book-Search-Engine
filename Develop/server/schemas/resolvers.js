3c56e0cc5f011947e3d9b109fc92505cf888fc9f
sthogue <sthogue16@gmail.com>
2023-11-05 16:44:16 -0600

    added Schemas folders, typeDefs and resolvers

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { Book , User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('thoughts');
+      }
+      throw AuthenticationError;
+    },
+  },
+  Mutation: {
+    // Takes in email and password as parameters and returns an Auth type.
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw AuthenticationError;
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw AuthenticationError;
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+    },
+    // Adds user to database and returns an Auth type. Takes in username, email, and password as parameters.
+    addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+    },
+    // Takes in book author, description, title, bookId, image, and link as parameters and returns a User type.
+    saveBook: async (parent, { book }, context) => {
+        if (context.user) {
+            const updatedUser = await User.findOneAndDelete(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            );
+            return updatedUser;
+        }
+        throw AuthenticationError;
+    },
+    // removes book from user and returns a User type. Takes in bookId as a parameter.
+    removeBook: async (parent, { bookId }, context) => {
+        if (context.user){
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+            return updatedUser;
+        }
+        throw AuthenticationError;
+    },
+  },
+};
